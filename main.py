f1d4c8b12cd26ce8d8584f12dc039b0efe92683c
BeJloHukku <igoryashamb@gmail.com>
2025-12-27 16:48:48 +0300

    feat: Created get-redis function and init for main RateLimiter class

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
-def main():
-    print("Hello from rate-limiter!")
+from functools import lru_cache
+
+from redis.asyncio import Redis
 
 
-if __name__ == "__main__":
-    main()
+@lru_cache
+def get_redis() -> Redis:
+    return Redis(host="localhost", port="6379")
+
+
+class RateLimiter:
+    def __init__(self, redis: Redis):
+        self._redis = redis
+
+    async def is_limited(
+            self,
+            ip_address: str,
+            endpoint: str,
+            max_requests: int,
+            window_seconds: int  
+) -> bool:
+        
